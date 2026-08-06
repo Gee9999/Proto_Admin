a0f2ac6732df8f2b95a73fc87c908491934ee7af
Gee <gee@users.noreply.github.com>
2026-08-06 22:39:03 +0200

    Fix image queue service imports

--- api/_image-processing-service.js
+++ api/_image-processing-service.js
@@ -7,6 +7,8 @@ import {
   claimImageObject,
   createPrivateSourceUrl,
   downloadPrivateSource,
+  readImageJob,
+  readImageJobIndex,
   releaseImageObjectClaim,
   saveAndIndexImageJob,
   storePrivateSource,
